54f62885771fcb15ff8ffed4f64988c8a7ddf460
alterra-muchson <52569071+muhammad-muchson@users.noreply.github.com>
2024-09-12 12:48:08 +0700

    introduction

---
+++ 01-introduction/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Dashboard from "@/components/dashboard-01";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Dashboard />
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
