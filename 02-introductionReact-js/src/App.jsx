2fe3522c899621cabbb48e552ba925b108a9e496
alterra-muchson <52569071+muhammad-muchson@users.noreply.github.com>
2024-09-12 16:48:09 +0700

    introduction react js

---
+++ 02-introductionReact-js/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+// import HeroSections from "./HeroSection";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-10xl font-bold underline">Hello world! </h1>
+    </>
+  );
+}
+
+export default App;
